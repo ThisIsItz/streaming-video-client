8ee3fa6ec600b2337efe62efa160182c4edd19af
ThisIsItz <itziarurbieta94@gmail.com>
2020-03-28 18:14:23 +0000

    Action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,6 +7,7 @@ import { SIGN_IN,
          DELETE_STREAM,
          EDIT_STREAM
         } from './types'
+import StreamShow from '../components/streams/StreamShow'
 
 export const signIn = (userId) => {
     return {
@@ -26,6 +27,42 @@ export const createStream = formValues => async dispatch => {
 
     dispatch({ 
         type: CREATE_STREAMS,
+        payload: response.data
+    })
+}
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams')
+
+    dispatch({
+        type: FETCH_STREAMS,
+        payload: response.data
+    })
+}
+
+export const fetchStream = (id) => async dispatch => {
+    const response = await streams.get(`/streams/${id}`)
+
+    dispatch({
+        type: FETCH_STREAM,
+        payload: response.data
+    })
+}
+
+export const deleteStream = (id) => async dispatch => {
+    await streams.delete(`/streams/${id}`)
+
+    dispatch({
+        type: DELETE_STREAM,
+        payload: id
+    })
+}
+
+export const editStream = (id, formValue) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formValue)
+
+    dispatch({
+        type: EDIT_STREAM,
         payload: response.data
     })
 }
