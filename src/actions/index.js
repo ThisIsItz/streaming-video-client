3a2e3772c27bd6e3231ef4a018a76860787295d9
ThisIsItz <itziarurbieta94@gmail.com>
2020-03-28 17:32:02 +0000

    Add action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,31 @@
+import streams from '../apis/streams'
+import { SIGN_IN,
+         SIGN_OUT,
+         CREATE_STREAMS,
+         FETCH_STREAM,
+         FETCH_STREAMS,
+         DELETE_STREAM,
+         EDIT_STREAM
+        } from './types'
+
 export const signIn = (userId) => {
     return {
-        type: 'SIGN_IN',
+        type: SIGN_IN,
         payload: userId
     }
 }
 
 export const signOut = () => {
     return {
-        type: 'SIGN_OUT'
+        type: SIGN_OUT
     }
+}
+
+export const createStream = formValues => async dispatch => {
+    const response = await streams.post('/streams', formValues)
+
+    dispatch({ 
+        type: CREATE_STREAMS,
+        payload: response.data
+    })
 }
