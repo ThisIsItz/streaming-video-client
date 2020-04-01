11b6cf2fbc8fcc4b3d4e72b0cb18ebdbe3f53149
ThisIsItz <itziarurbieta94@gmail.com>
2020-04-01 11:07:03 +0100

    Refactored Edit Stream

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import _ from 'lodash'
 import { connect } from 'react-redux'
 import { fetchStream, editStream } from '../../actions'
 import StreamForm from './streamForm'   
@@ -20,7 +21,7 @@ class StreamEdit extends React.Component {
         return (
             <div>
                 <h3>Edit a Stream</h3>
-                <StreamForm initialValues={this.props.stream} onSubmit={this.onSubmit}/>
+                <StreamForm initialValues={_.pick(this.props.stream, 'title', 'description')} onSubmit={this.onSubmit}/>
             </div>
         )
     }
